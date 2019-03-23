6ca83f0791b72a53eb2c8db892d7c69608d16b25
unknown <lappjeff20@gmail.com>
2019-03-23 14:33:27 -0400

    Finished setup for project

--- Material-UI-Projects/project-1/src/App.js
+++ Material-UI-Projects/project-1/src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import ReactDom from 'react-dom'
+
+//Material UI imports
+import Button from '@material-ui/core/Button'
+import Icon from '@material-ui/core/Icon'
+
+//Style sheet import
+import './App.scss'
+
+//Custom Component imports
+import { Header, Footer } from './components/Layouts'
+import Exercises from './components/Exercises'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Header />
+
+        <Exercises />
+        
+        <Footer/>
+      </Fragment>
     );
   }
 }
